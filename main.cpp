e86a295816f5bf112683ca4af4ce6da73a2a9113
吉田彪 <@MSI>
2023-09-12 21:11:04 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int scene = 0;
+	switch (scene)
+	{
+	case0:
+		break;
+	case1:
+		break;
+	case2:
+		break; 
+	case3:
+		break;
+	case4:
+		break;
+	case5:
+		break;
+	}
+
+}
